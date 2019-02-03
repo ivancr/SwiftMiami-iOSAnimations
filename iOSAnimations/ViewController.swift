761c28f268c106a5f7377898de6303225bcffb9e
Iván <digitalp@me.com>
2019-02-02 21:18:52 -0500

    Initial Commit

---
+++ iOSAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAnimations
+//
+//  Created by Ivan Corchado Ruiz on 2/2/19.
+//  Copyright © 2019 ivancr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
